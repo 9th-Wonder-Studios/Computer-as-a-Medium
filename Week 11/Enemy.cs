28f7bb95231dda7c04119c89a2f49fdbe8faec03
Chandler Guzman <ninthewander@gmail.com>
2025-11-01 13:40:24 -0400

    finished week 11 assignment
    also added arrow.gif for the README

--- Week 11/Enemy.cs
+++ Week 11/Enemy.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private bool spawnLeft = false;
+    private bool spawnRight = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* First, the spawn position must be determined since the movement pattern will change
+        depending on if the enemy spawned in the top left or top right. */
+        if (transform.position.x == -9f)
+        {
+            spawnLeft = true;
+        }
+        else if (transform.position.x == 9f)
+        {
+            spawnRight = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+        if (spawnLeft == true)
+        {
+            // If the enemy spawned in the top left, it will move down and to the right consistently.
+            transform.Translate(new Vector3(1f, -1f, 0f) * Time.deltaTime * 1.5f);
+        }
+        else if (spawnRight == true)
+        {
+            // If the enemy spawned in the top right, it will move down and to the left consistently.
+            transform.Translate(new Vector3(-1f, -1f, 0f) * Time.deltaTime * 1.5f);
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
+        }
+
         if (transform.position.y < -6.5f)
         {
             Destroy(this.gameObject);
--- Week 11/GameManager.cs
+++ Week 11/GameManager.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     public GameObject enemyOnePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1, 2);
+        InvokeRepeating("CreateEnemyTwo", 2, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
     void CreateEnemyOne()
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyTwo()
+    {
+        // Range is between 0 and 2 since 2 is exclusive.
+        int startingPos = Random.Range(0, 2);
+
+        // Depending on the number chosen by startingPos, the enemy will spawn in a different corner of the scene.
+        if (startingPos == 0)
+        {
+            Instantiate(enemyOnePrefab, new Vector3(-9f, 6.5f, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemyOnePrefab, new Vector3(9f, 6.5f, 0), Quaternion.identity);
+        }
     }
 }
